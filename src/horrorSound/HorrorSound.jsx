5efdf27e5f13e5902844f27629b6bc94160c20d8
Chan-sol <80556770+Chan-sol@users.noreply.github.com>
2021-06-26 15:24:08 +0900

    feat: horror sound 구현

--- src/horrorSound/HorrorSound.jsx
+++ src/horrorSound/HorrorSound.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useSound from "use-sound";
 import styles from "./HorrorSound.module.css";
+import backgroundAudioPath from './SoundTrack/2.mp3';
+
 
 function HorrorSound() {
-  return <div></div>;
+  const SoundEffect = new Audio(".mp3");
+  const startSoundEffect = () => {
+    SoundEffect.play();
+  }
+
+  const [, {sound}] = useSound(backgroundAudioPath, { volume: 0.25 });
+  useEffect(() => {
+    if(sound===null) return;
+    sound._loop = true;
+    console.log(sound);
+    sound.play();
+  }, [sound]);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.blur}/>
+      </div>
+      <button className={styles.btn} onclick={startSoundEffect}>doll</button>
+    </>
+  );
 }
 
 export default HorrorSound;
