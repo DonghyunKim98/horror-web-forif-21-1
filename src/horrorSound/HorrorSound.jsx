7681227c578ad11cfc9f98ce754cb8d6f3702d33
Kim Dong Hyun <akainoo123@gmail.com>
2021-06-26 09:32:45 +0900

    feat: add default components to cooperate

---
+++ src/horrorSound/HorrorSound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HorrorSound.module.css";
+
+function HorrorSound() {
+  return <div></div>;
+}
+
+export default HorrorSound;
---
+++ src/horrorStory/horrorStory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "horrorStory.module.css";
+
+function horrorStory() {
+  return <div></div>;
+}
+
+export default horrorStory;
