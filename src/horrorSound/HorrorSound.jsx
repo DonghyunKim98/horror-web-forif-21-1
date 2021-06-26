1c718b3cbc038985da165513b4215f2d1a287b33
Chan-sol <80556770+Chan-sol@users.noreply.github.com>
2021-06-26 18:01:26 +0900

    feat: make horrorsound tracks

--- src/horrorSound/HorrorSound.jsx
+++ src/horrorSound/HorrorSound.jsx
@@ -2,28 +2,49 @@ import React, { useEffect } from "react";
 import useSound from "use-sound";
 import styles from "./HorrorSound.module.css";
 import backgroundAudioPath from './SoundTrack/2.mp3';
-
+import laughEffectPath from './SoundTrack/laugh.mp3';
+import breakwindowEffectPath from './SoundTrack/break.mp3';
+import drawerEffectPath from './SoundTrack/drawer.mp3';
+import ropeEffectPath from './SoundTrack/rope.mp3';
 
 function HorrorSound() {
-  const SoundEffect = new Audio(".mp3");
-  const startSoundEffect = () => {
-    SoundEffect.play();
+  const laughSoundEffect = new Audio(laughEffectPath);
+  const breakwindowSoundEffect = new Audio(breakwindowEffectPath);
+  const drawerSoundEffect = new Audio(drawerEffectPath);
+  const ropeSoundEffect = new Audio(ropeEffectPath);
+
+  const laugh = () => {
+    laughSoundEffect.play();
+  } 
+  const breakwindow = () => {
+    breakwindowSoundEffect.play();
+  }
+  const drawer = () => {
+    drawerSoundEffect.play();
+  }
+  const rope = () => {
+    ropeSoundEffect.play();
   }
 
   const [, {sound}] = useSound(backgroundAudioPath, { volume: 0.25 });
   useEffect(() => {
     if(sound===null) return;
     sound._loop = true;
-    console.log(sound);
     sound.play();
   }, [sound]);
+
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.blur}/>
       </div>
-      <button className={styles.btn} onclick={startSoundEffect}>doll</button>
+      <div className={styles.buttons}>
+        <button className={styles.laughbtn} onClick={laugh}>doll</button>
+        <button className={styles.breakbtn} onClick={breakwindow}>break</button>
+        <button className={styles.drawerbtn} onClick={drawer}>drawer</button>
+        <button className={styles.ropebtn} onClick={rope}>rope</button>
+      </div>
     </>
   );
 }
